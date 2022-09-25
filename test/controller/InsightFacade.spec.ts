eeb2d588989f536a0d9348af4df326555556aa93
UBC Student <parkerl6@students.cs.ubc.ca>
2022-09-25 16:52:07 -0700

    ISSUE-0: Merge Parker's C0 tests (#3)
    * Added 'addDataset' tests
    
    * Added 'listDataset' tests
    
    * Added 'removeDataset' tests
    
    * Added 'performQuery' tests
    
    Co-authored-by: Parker Lee <parkerlee247@gmail.com>

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -1,15 +1,16 @@
 import {
+	InsightDataset,
 	InsightDatasetKind,
 	InsightError,
 	InsightResult,
-	ResultTooLargeError
+	NotFoundError,
+	ResultTooLargeError,
 } from "../../src/controller/IInsightFacade";
 import InsightFacade from "../../src/controller/InsightFacade";
-
 import * as fs from "fs-extra";
-
 import {folderTest} from "@ubccpsc310/folder-test";
 import {expect} from "chai";
+import {it} from "mocha";
 
 describe("InsightFacade", function () {
 	let insightFacade: InsightFacade;
@@ -17,7 +18,7 @@ describe("InsightFacade", function () {
 	const persistDirectory = "./data";
 	const datasetContents = new Map<string, string>();
 
-	// Reference any datasets you've added to test/resources/archives here and they will
+	// Reference any datasets you've added to test/resources/archives here, and they will
 	// automatically be loaded in the 'before' hook.
 	const datasetsToLoad: {[key: string]: string} = {
 		sections: "./test/resources/archives/pair.zip",
@@ -33,21 +34,22 @@ describe("InsightFacade", function () {
 		fs.removeSync(persistDirectory);
 	});
 
-	describe("Add/Remove/List Dataset", function () {
-		before(function () {
-			console.info(`Before: ${this.test?.parent?.title}`);
-		});
+	describe("addDataset / listDataset / removeDataset", function () {
+		const id: string = "sections";
+		const content: string = datasetContents.get("sections") ?? "";
+
+		// before(function () {
+		// 	console.info(`Before: ${this.test?.parent?.title}`);
+		// });
 
 		beforeEach(function () {
-			// This section resets the insightFacade instance
-			// This runs before each test
 			console.info(`BeforeTest: ${this.currentTest?.title}`);
 			insightFacade = new InsightFacade();
 		});
 
-		after(function () {
-			console.info(`After: ${this.test?.parent?.title}`);
-		});
+		// after(function () {
+		// 	console.info(`After: ${this.test?.parent?.title}`);
+		// });
 
 		afterEach(function () {
 			// This section resets the data directory (removing any cached data)
@@ -56,13 +58,162 @@ describe("InsightFacade", function () {
 			fs.removeSync(persistDirectory);
 		});
 
-		// This is a unit test. You should create more like this!
-		it("Should add a valid dataset", function () {
-			const id: string = "sections";
-			const content: string = datasetContents.get("sections") ?? "";
+		it("Should add a valid dataset", async function () {
 			const expected: string[] = [id];
-			return insightFacade.addDataset(id, content, InsightDatasetKind.Sections)
-				.then((result: string[]) => expect(result).to.deep.equal(expected));
+			const result = await insightFacade.addDataset(id, content, InsightDatasetKind.Sections);
+
+			expect(result).to.deep.equal(expected);
+		});
+
+		it("Should add multiple valid datasets", async function () {
+			const id2: string = "sections2";
+			const content2: string = datasetContents.get("sections") ?? "";
+			const expected: string[] = [id, id2];
+			await insightFacade.addDataset(id, content, InsightDatasetKind.Sections);
+			const result = await insightFacade.addDataset(id2, content2, InsightDatasetKind.Sections);
+
+			expect(result).to.deep.equal(expected);
+		});
+
+		it("Should reject datasets with underscores in id", async function () {
+			const invalidId: string = "sections_invalid";
+			try {
+				await insightFacade.addDataset(invalidId, content, InsightDatasetKind.Sections);
+				expect.fail("Should not have resolved");
+			} catch (err) {
+				expect(err).to.be.instanceof(InsightError);
+			}
+		});
+
+		it("Should reject datasets with only whitespaces in id", async function () {
+			const invalidId: string = " ";
+			try {
+				await insightFacade.addDataset(invalidId, content, InsightDatasetKind.Sections);
+				expect.fail("Should not have resolved");
+			} catch (err) {
+				expect(err).to.be.instanceof(InsightError);
+			}
+		});
+
+		it("Should reject datasets already on disk", async function () {
+			await insightFacade.addDataset(id, content, InsightDatasetKind.Sections);
+			try {
+				await insightFacade.addDataset(id, content, InsightDatasetKind.Sections);
+				expect.fail("Should not have resolved");
+			} catch (err) {
+				expect(err).to.be.instanceof(InsightError);
+			}
+		});
+
+		it("Should list no datasets when there is nothing on disk", async function () {
+			const expected: InsightDataset[] = [];
+			const result = await insightFacade.listDatasets();
+
+			expect(result).to.deep.equal(expected);
+		});
+
+		it("Should list a single dataset", async function () {
+			await insightFacade.addDataset(id, content, InsightDatasetKind.Sections);
+			const expected: InsightDataset[] = [
+				{
+					id: "sections",
+					kind: InsightDatasetKind.Sections,
+					numRows: 64612,
+				},
+			];
+			const result = await insightFacade.listDatasets();
+
+			expect(result).to.deep.equal(expected);
+		});
+
+		it("Should list multiple datasets", async function () {
+			const id2: string = "sections2";
+			const content2: string = datasetContents.get("sections") ?? "";
+			await insightFacade.addDataset(id, content, InsightDatasetKind.Sections);
+			await insightFacade.addDataset(id2, content2, InsightDatasetKind.Sections);
+			const expected1: InsightDataset = {
+				id: "sections",
+				kind: InsightDatasetKind.Sections,
+				numRows: 64612,
+			};
+			const expected2: InsightDataset = {
+				id: "sections2",
+				kind: InsightDatasetKind.Sections,
+				numRows: 64612,
+			};
+			const result = await insightFacade.listDatasets();
+
+			expect(result).to.be.instanceof(Array);
+			expect(result).to.have.length(2);
+			expect(result).to.deep.include(expected1);
+			expect(result).to.deep.include(expected2);
+		});
+
+		it("Should remove a single dataset", async function () {
+			await insightFacade.addDataset(id, content, InsightDatasetKind.Sections);
+			const expected: InsightDataset = {
+				id: "sections",
+				kind: InsightDatasetKind.Sections,
+				numRows: 64612,
+			};
+			const result = await insightFacade.removeDataset(id);
+
+			expect(result).to.equal(id);
+			expect(await insightFacade.listDatasets()).to.not.deep.include(expected);
+		});
+
+		it("Should remove multiple datasets", async function () {
+			const id2: string = "sections2";
+			const content2: string = datasetContents.get("sections") ?? "";
+			await insightFacade.addDataset(id, content, InsightDatasetKind.Sections);
+			await insightFacade.addDataset(id2, content2, InsightDatasetKind.Sections);
+			const expected1: InsightDataset = {
+				id: "sections",
+				kind: InsightDatasetKind.Sections,
+				numRows: 64612,
+			};
+			const expected2: InsightDataset = {
+				id: "sections",
+				kind: InsightDatasetKind.Sections,
+				numRows: 64612,
+			};
+
+			let result = await insightFacade.removeDataset(id);
+			expect(result).to.equal(id);
+			expect(await insightFacade.listDatasets()).to.not.deep.include(expected1);
+
+			result = await insightFacade.removeDataset(id2);
+			expect(result).to.equal(id2);
+			expect(await insightFacade.listDatasets()).to.not.deep.include(expected2);
+		});
+
+		it("Should reject when dataset cannot be found", async function () {
+			try {
+				await insightFacade.removeDataset(id);
+				expect.fail("Should not have resolved");
+			} catch (err) {
+				expect(err).to.be.instanceof(NotFoundError);
+			}
+		});
+
+		it("Should reject when given a dataset id with underscores", async function () {
+			const invalidId: string = "sections_invalid";
+			try {
+				await insightFacade.removeDataset(invalidId);
+				expect.fail("Should not have resolved");
+			} catch (err) {
+				expect(err).to.be.instanceof(InsightError);
+			}
+		});
+
+		it("Should reject when given a dataset id with only whitespaces", async function () {
+			const invalidId: string = " ";
+			try {
+				await insightFacade.removeDataset(invalidId);
+				expect.fail("Should not have resolved");
+			} catch (err) {
+				expect(err).to.be.instanceof(InsightError);
+			}
 		});
 	});
 
@@ -71,7 +222,7 @@ describe("InsightFacade", function () {
 	 * You should not need to modify it; instead, add additional files to the queries directory.
 	 * You can still make tests the normal way, this is just a convenient tool for a majority of queries.
 	 */
-	describe("PerformQuery", () => {
+	describe("performQuery", () => {
 		before(function () {
 			console.info(`Before: ${this.test?.parent?.title}`);
 
@@ -98,7 +249,7 @@ describe("InsightFacade", function () {
 		type PQErrorKind = "ResultTooLargeError" | "InsightError";
 
 		folderTest<unknown, Promise<InsightResult[]>, PQErrorKind>(
-			"Dynamic InsightFacade PerformQuery tests",
+			"Dynamic InsightFacade performQuery tests",
 			(input) => insightFacade.performQuery(input),
 			"./test/resources/queries",
 			{
